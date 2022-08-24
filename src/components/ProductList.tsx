807a6be67289099ff0a6b7f54c8c1cd89c8a2035
rafael.monteiro <rafael.monteiro@segfy.com>
2022-08-24 15:16:50 -0300

    first commit

---
+++ src/components/ProductList.tsx
@@ -0,0 +1,12 @@
+function ProductList({ products }) {
+    return (
+      <ul>
+        {products.map((product) => (
+          <li>{product}</li>
+        ))}
+      </ul>
+    );
+  }
+  
+  export default ProductList;
+  
